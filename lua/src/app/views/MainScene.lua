aaac39cb0dfc526ef38875cf1774770e4b23ad49
Jimmy Yin <jimmy.yin5@gmail.com>
2017-01-13 15:57:38 +0800

    update lua

--- lua/src/app/views/MainScene.lua
+++ lua/src/app/views/MainScene.lua
@@ -20,29 +20,50 @@ end
 function MainScene:setupTestMenu()
     local menu = cc.Menu:create()
 
-    local label1 = cc.Label:createWithSystemFont("Test Item 1", "sans", 28)
+    local label1 = cc.Label:createWithSystemFont("show banner", "sans", 28)
     local item1 = cc.MenuItemLabel:create(label1)
     item1:onClicked(function()
-        print("Test Item 1")
+        sdkbox.PluginPhunwareAds:show("banner")
     end)
     menu:addChild(item1)
 
-    local label2 = cc.Label:createWithSystemFont("Test Item 2", "sans", 28)
+    local label2 = cc.Label:createWithSystemFont("show interstitial", "sans", 28)
     local item2 = cc.MenuItemLabel:create(label2)
     item2:onClicked(function()
-        print("Test Item 2")
+        sdkbox.PluginPhunwareAds:show("banner")
     end)
     menu:addChild(item2)
 
-    local label3 = cc.Label:createWithSystemFont("Test Item 3", "sans", 28)
+    local label3 = cc.Label:createWithSystemFont("show reward", "sans", 28)
     local item3 = cc.MenuItemLabel:create(label3)
     item3:onClicked(function()
-        print("Test Item 3")
+        sdkbox.PluginPhunwareAds:show("banner")
     end)
     menu:addChild(item3)
 
+    local label4 = cc.Label:createWithSystemFont("show page", "sans", 28)
+    local item4 = cc.MenuItemLabel:create(label4)
+    item3:onClicked(function()
+        sdkbox.PluginPhunwareAds:show("banner")
+    end)
+    menu:addChild(item4)
+
+    local label5 = cc.Label:createWithSystemFont("show video", "sans", 28)
+    local item5 = cc.MenuItemLabel:create(label5)
+    item3:onClicked(function()
+        sdkbox.PluginPhunwareAds:show("banner")
+    end)
+    menu:addChild(item5)
+
     menu:alignItemsVerticallyWithPadding(24)
     self:addChild(menu)
+
+    --
+    sdkbox.PluginPhunwareAds:setUserID("lua123")
+    sdkbox.PluginPhunwareAds:init()
+    sdkbox.PluginPhunwareAds:setListener(function ( args )
+        dump(args)
+    end)
 end
 
 return MainScene
