51968f4b23e8f7fc07e6ce62bdb728c6f8d23de4
Jimmy Yin <jimmy.yin5@gmail.com>
2017-01-13 16:12:58 +0800

    update js

--- js/src/app.js
+++ js/src/app.js
@@ -33,26 +33,52 @@ var HelloWorldLayer = cc.Layer.extend({
     createTestMenu:function() {
         var menu = new cc.Menu();
 
-        var item1 = new cc.MenuItemLabel(new cc.LabelTTF("Test Item 1", "sans", 28), function() {
-            cc.log("Test Item 1");
+        var item1 = new cc.MenuItemLabel(new cc.LabelTTF("banner", "sans", 28), function() {
+            sdkbox.PluginPhunwareAds.show("banner");
         });
         menu.addChild(item1);
 
-        var item2 = new cc.MenuItemLabel(new cc.LabelTTF("Test Item 2", "sans", 28), function() {
-            cc.log("Test Item 2");
+        var item2 = new cc.MenuItemLabel(new cc.LabelTTF("interstitial", "sans", 28), function() {
+            sdkbox.PluginPhunwareAds.show("interstitial");
         });
         menu.addChild(item2);
 
-        var item3 = new cc.MenuItemLabel(new cc.LabelTTF("Test Item 3", "sans", 28), function() {
-            cc.log("Test Item 3");
+        var item3 = new cc.MenuItemLabel(new cc.LabelTTF("reward", "sans", 28), function() {
+            sdkbox.PluginPhunwareAds.show("reward");
         });
         menu.addChild(item3);
+
+        var item4 = new cc.MenuItemLabel(new cc.LabelTTF("page", "sans", 28), function() {
+            sdkbox.PluginPhunwareAds.show("page");
+        });
+        menu.addChild(item4);
+
+        var item5 = new cc.MenuItemLabel(new cc.LabelTTF("video", "sans", 28), function() {
+            sdkbox.PluginPhunwareAds.show("video");
+        });
+        menu.addChild(item5);
 
         var winsize = cc.winSize;
         menu.x = winsize.width / 2;
         menu.y = winsize.height / 2;
         menu.alignItemsVerticallyWithPadding(20);
         this.addChild(menu);
+
+        //
+        sdkbox.PluginPhunwareAds.setUserID("js123");
+        sdkbox.PluginPhunwareAds.init();
+        sdkbox.PluginPhunwareAds.setListener({
+
+            adLoaded : function(name) { cc.log("adLoaded " + name) },
+            adFailed : function(name, errorCode, msg) { cc.log("adFailed " + name + " " + msg) },
+            adWillPresent : function(name) { cc.log("adWillPresent " + name) },
+            adDidPresent : function(name) { cc.log("adDidPresent " + name) },
+            adWillDissmiss : function(name) { cc.log("adWillDissmiss " + name) },
+            adDidDismiss : function(name) { cc.log("adDidDismiss " + name) },
+            willLeaveApp : function(name) { cc.log("willLeaveApp " + name)},
+            reward : function(name, amount, currency) { cc.log("reward " + name) },
+
+        });
     }
 });
 
